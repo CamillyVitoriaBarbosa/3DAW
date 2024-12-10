5a7cef68f861919affdd585d0d82d8911d8d9fd1
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-12-10 08:08:23 -0300

    Update conexao.php

--- Projeto Hostel/adm/conexao.php
+++ Projeto Hostel/adm/conexao.php
@@ -1,6 +1,11 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hostelalmeidao";
+
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=hostelalmeidao', 'root', '');
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
